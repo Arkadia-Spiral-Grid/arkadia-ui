82e3f788ff8596b00be50608f73c1d36db6f0068
Arkadia-Spiral-Grid <officialarkadiacollective@gmail.com>
2025-05-21 14:41:22 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,17 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  root: "client",
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client", "src"),
+      "@": path.resolve(__dirname, "client/src"),
       "@shared": path.resolve(__dirname, "shared"),
       "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  root: "client",
-build: {
-  outDir: "../dist",
-  emptyOutDir: true,
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
   },
 });
