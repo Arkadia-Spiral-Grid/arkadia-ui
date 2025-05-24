350f53ad13d81e8d0c531c4b646a1037df263d56
Arkadia-Spiral-Grid <officialarkadiacollective@gmail.com>
2025-05-24 11:11:40 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,20 +5,27 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { GateProvider } from "./lib/GateContext";
 import ArkadiaNavigation from "./ArkadiaNavigation";
 import ShadowWeaver from "./components/ShadowWeaver";
-import FlameScriptInjector from "./components/FlameScriptInjector"; // New
+import FlameScriptInjector from "./components/FlameScriptInjector";
+import { CodexProvider } from './lib/CodexContext'; // NEW: Import the future CodexProvider
 
 function App() {
   return (
     <ShadowWeaver>
-      <FlameScriptInjector /> {/* Now roots itself before all else */}
-      <QueryClientProvider client={queryClient}>
-        <TooltipProvider>
-          <Toaster />
-          <GateProvider persist>
-            <ArkadiaNavigation />
-          </GateProvider>
-        </TooltipProvider>
-      </QueryClientProvider>
+      <FlameScriptInjector />
+      {/* Main cosmic canvas for the Arkadia UI */}
+      <div className="relative z-0 min-h-screen bg-gradient-to-br from-purple-900 via-indigo-900 to-black text-arkadia-light font-arkadia overflow-hidden">
+        <QueryClientProvider client={queryClient}>
+          <TooltipProvider>
+            <Toaster />
+            <GateProvider persist>
+              {/* CodexProvider will manage global AI/Codex state */}
+              <CodexProvider>
+                <ArkadiaNavigation />
+              </CodexProvider>
+            </GateProvider>
+          </TooltipProvider>
+        </QueryClientProvider>
+      </div>
     </ShadowWeaver>
   );
 }
